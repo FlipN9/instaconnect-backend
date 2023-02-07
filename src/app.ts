2718ab43e18d59dc146daa08e14cf27c46f372df
FlipN9 <yj2243@nyu.edu>
2023-02-06 23:43:16 -0500

    auth signup

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
-import express, { Express } from "express";
-import { InstaconnectServer } from "./setupServer";
-import databaseConnection from "./setupDatabase";
+import express, { Express } from 'express';
+import { InstaconnectServer } from '@root/setupServer';
+import databaseConnection from '@root/setupDatabase';
+import { config } from '@root/config';
 
 class Application {
-  public initialize(): void {
-    databaseConnection();
-    const app: Express = express();
-    const server: InstaconnectServer = new InstaconnectServer(app);
-    server.start();
-  }
+    public initialize(): void {
+        this.loadConfig();
+        databaseConnection();
+        const app: Express = express();
+        const server: InstaconnectServer = new InstaconnectServer(app);
+        server.start();
+    }
+
+    private loadConfig(): void {
+        config.validateConfig();
+        config.cloudinaryConfig();
+    }
 }
 
 const application: Application = new Application();
--- src/setupDatabase.ts
+++ src/setupDatabase.ts
@@ -1,22 +1,22 @@
-import mongoose from "mongoose";
-import Logger from "bunyan";
-import { config } from "./config";
+import mongoose from 'mongoose';
+import Logger from 'bunyan';
+import { config } from '@root/config';
 
-const log: Logger = config.createLogger("setupDatabase");
+const log: Logger = config.createLogger('setupDatabase');
 
 export default () => {
-  mongoose.set("strictQuery", true);
-  const connect = () => {
-    mongoose
-      .connect(`${config.DATABASE_URL}`)
-      .then(() => {
-        log.info("Successfully connected to database.");
-      })
-      .catch((error) => {
-        log.error("Error connecting to database", error);
-        return process.exit(1);
-      });
-  };
-  connect();
-  mongoose.connection.on("disconnected", connect);
+    mongoose.set('strictQuery', true);
+    const connect = () => {
+        mongoose
+            .connect(`${config.DATABASE_URL}`)
+            .then(() => {
+                log.info('Successfully connected to database.');
+            })
+            .catch((error) => {
+                log.error('Error connecting to database', error);
+                return process.exit(1);
+            });
+    };
+    connect();
+    mongoose.connection.on('disconnected', connect);
 };
